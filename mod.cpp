1c24e87c7b9006179ea302101c865985c4683141
Sparfell <arsparfell19@gmail.com>
2019-01-08 09:55:34 +0100

    Ajout logo fait par Goya en 2 versions

---
+++ mod.cpp
@@ -0,0 +1,18 @@
+name = "GDC LIB";
+picture = "gdc_lib_main\data\logo_big.paa";
+logoSmall = "gdc_lib_main\data\logo.paa";
+logo = "gdc_lib_main\data\logo.paa";
+logoOver = "gdc_lib_main\data\logo_big.paa";
+actionName = "GitHub";
+action = "https://github.com/GdC-Framework/GdC_lib/wiki";
+tooltipOwned = "GDC LIB";
+dlcColor[] = 
+{
+	0.23,
+	0.39,
+	0.30,
+	1
+};
+overview = "gdc_lib est un addon crée par les membres de la communauté ""Grèce de Canards"" qui a pour objectif de fournir divers outils pour les créateurs de missions.";
+hideName = 0;
+hidePicture = 0;
