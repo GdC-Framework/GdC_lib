8e5ec84111d80006ff467f60eaea178b35070f74
Migoyan <64397268+Migoyan@users.noreply.github.com>
2022-09-04 00:00:36 +0200

    Addon mod configs creation (#111)

---
+++ gdc_lib_modConfigs/config.cpp
@@ -0,0 +1,11 @@
+class CfgPatches
+{
+    class gdc_lib_modConfigs
+    {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 0.1;
+        requiredAddons[] = {"A3_Data_F"};
+    };
+};
+EnableTargetDebug=1;
