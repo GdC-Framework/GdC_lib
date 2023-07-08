63720e7e5e9884b9a7e7ed02172a0e40a5d3ee4a
Sparfell <arsparfell19@gmail.com>
2023-07-08 18:48:50 +0200

    replaced legacy bft

--- gdc_lib_main/functions/gdc_bft/index.hpp
+++ gdc_lib_main/functions/gdc_bft/index.hpp
@@ -3,7 +3,6 @@ class gdc_bft
 {
     file = "gdc_lib_main\functions\gdc_bft";
     class bft {};
-    class bftInit {};
     class bftDrawMarkers {};
     class bftUpdateList {};
     class bftui {};
